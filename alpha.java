272aca4b80f631f017df70c63f67d5eabff73bef
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-13 12:49:23 +0530

    Add files via upload

---
+++ alpha.java
@@ -0,0 +1,18 @@
+
+
+ class alp {
+
+	public static void main(String[] args) {
+		char c='a';
+
+		if((c>='a'&&c<='z')||(c>='A'&&c<='Z')){
+		System.out.println(" alphabet");
+		}
+		else{
+			System.out.println("not a alphabet");
+		}
+		
+
+	}
+
+}
