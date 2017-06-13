260133a9684a47e03c4b0c2fade6155989eacd86
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-13 12:22:11 +0530

    Add files via upload

---
+++ vowelconst.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("\n enter the character:");
+scanf("%c",&ch);
+switch(ch)
+{
+case 'a':
+printf("\n vowel:");
+break;
+case 'b':
+printf("\n consonant:");
+break;
+default:
+printf("\n invalid data:");
+break;
+ }
+ }
