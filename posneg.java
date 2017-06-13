7b597ebde9cc0e3aa423e90d8845d6f621a16ff5
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-13 12:10:03 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,19 @@
+
+
+ class pn {
+
+	public static void main(String[] args) {
+		int a=-10;
+		if(a>1){
+			System.out.println("positive");
+		}
+		else if(a<0){
+			System.out.println("negative");
+		}
+		else{
+			System.out.println("zero");
+		}
+
+	}
+
+}
