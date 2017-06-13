ac4dc66d386fcf173a1d74ba7171182c4e248010
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-13 12:13:02 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,19 @@
+
+class num {
+
+	public static void main(String[] args) {
+		
+Scanner even=new Scanner (System.in);
+int s=even.nextInt();
+if(s%2==0){
+	System.out.println("even num");
+}
+	else
+	{
+		System.out.println("odd num");
+}
+	
+	
+	}
+
+}
