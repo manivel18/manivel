aa49f78c3fcd0b936546510448216081791a5c9d
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-13 14:30:33 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+printf("\n enter the year:");
+scanf("%d",&year);
+if(year%4==0)
+{
+printf("\n leap year");
+}
+else
+{
+printf("\n not a leap year");
+}
+}
