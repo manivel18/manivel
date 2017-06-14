c4b3ac090238b91cc1f30db7094cf542c80d56ae
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-14 15:30:36 +0530

    Add files via upload

---
+++ countdig.java
@@ -0,0 +1,13 @@
+package count;
+
+public class countdig {
+	public static void main(String[] args)
+	{int a=5678,c=0;
+	while(a!=0)
+	{a/=10;
+	++c;
+	
+	}System.out.println("no.of digit:"+c);
+	}
+
+}
