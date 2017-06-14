a59c38ab11da7a3f6033f352d2a56aa0c9dea3dc
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-14 14:42:45 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,13 @@
+package factorial;
+
+public class fact {
+
+	public static void main(String[] args) {
+		int i,b=5,factorial=1;
+		for(i=1;i<=5;i++){
+			factorial=factorial*i;
+		}
+System.out.println(+factorial);
+	}
+
+}
