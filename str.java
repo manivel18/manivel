24f6da1c42abf4c75d2f51993ad02ed24ce197f3
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-14 16:44:10 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,13 @@
+package string;
+
+public class str {
+
+	public static void main(String[] args) {
+		String s1="hello";
+		String s2="world";
+		String s3=s2+s1;
+		System.out.println(s3);
+
+	}
+
+}
