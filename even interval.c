7100e358b17b6424d67e14b88af382fe89579cee
manivel18 <manivel.nece@ksrce.ac.in>
2017-06-14 12:25:09 +0530

    Add files via upload

---
+++ even interval.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+
+int main ()
+
+{
+    clrscr();
+    int num,m =20,n=40;
+
+
+
+    printf("Print even Numbers in a given range m to n:\n");
+
+    for (num = m; num <= n; num++)
+
+        {
+
+               if (num %2 == 0);
+
+                  printf ("%d ", num);
+
+         }
+}
+
+
+
